dbd53cf4567b5d32b4cf9293a491e5c2334dd066
Rodrigo Miyashiro Mucci <Rodrigo_miyashiro@hotmail.com>
2019-05-21 17:20:06 -0300

    Add files via upload

---
+++ reposition/src/main/java/br/edu/utfpr/group/reposition/RepositionApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.group.reposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositionApplication.class, args);
+	}
+
+}
