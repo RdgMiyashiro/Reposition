0a3d8362405f1617409df0b98cb73a130bd7e38a
Rodrigo Miyashiro Mucci <Rodrigo_miyashiro@hotmail.com>
2019-05-22 12:50:02 -0300

    Class business model validations!

--- reposition/src/main/java/br/edu/utfpr/group/reposition/entity/Aula.java
+++ reposition/src/main/java/br/edu/utfpr/group/reposition/entity/Aula.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.utfpr.group.reposition.entity;
 
+import br.edu.utfpr.group.reposition.dao.CalendarioDAO;
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,16 +12,16 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  *
  * @author Jéssica Ayumi Uehara
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+@Getter
+@Setter
 @Builder
 @Entity
 public class Aula implements Serializable {
@@ -35,10 +32,54 @@ public class Aula implements Serializable {
     private Date data_reposicao;
     private String conteudo;
     
-    @ManyToOne
+    @ManyToOne 
     private Reposicao reposicao;
     
     @ManyToOne
     private Materia materia;
+
+    public Aula(Date data, Date dataInit, Date dataFin, Date dataAusencia, String conteudo, Materia materia, Reposicao reposicao) throws Exception {
+        
+        validaData(data, dataInit, dataFin, dataAusencia);
+        setConteudo(conteudo);
+        setMateria(materia);
+        setReposicao(reposicao);
+    }
     
+    public void dataPosAusencia(Date data) throws Exception {
+        Date dAtual = new Date();
+        if (dAtual.after(data))
+            throw new Exception("Data invalida");
+    }
+    
+    public void dataAposQuinze(Date data) throws Exception {
+        Date dLimite = Aula.dataLimite(data, 15);
+        
+        if (data.after(dLimite))
+            throw new Exception("Data invalida");
+    }
+    
+    public void dataValidaCalendario(Date data, Date dataInit, Date dataFin) throws Exception {
+        if (data.before(dataInit))
+            throw new Exception("Data invalida");
+        
+        if (data.after(dataFin))
+            throw new Exception("Data invalida");
+
+    }
+    
+    private void validaData(Date data, Date dataInit, Date dataFin, Date dataAusencia) throws Exception{
+        dataPosAusencia(dataAusencia);
+        dataAposQuinze(data);
+        dataValidaCalendario(data, dataInit, dataFin);
+        
+        setData_reposicao(data);
+    }
+    
+    private static Date dataLimite(Date date, int dias) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, dias);
+        return cal.getTime();
+    }
 }
